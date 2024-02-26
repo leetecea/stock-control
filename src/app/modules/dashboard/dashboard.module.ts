8d291818594d3e9ff77470a0f03dfab7540bfd14
Letícia Costa <leticia_603295@trt5.jus.br>
2024-02-26 11:40:07 -0300

    iniciando componente toolbar

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -12,7 +12,8 @@ import { CardModule } from 'primeng/card';
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
 import { CookieService } from 'ngx-cookie-service';
-
+import { ChartModule} from 'primeng/chart'
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 
@@ -25,11 +26,15 @@ import { CookieService } from 'ngx-cookie-service';
     FormsModule,
     ReactiveFormsModule,
     RouterModule.forChild(DASHBOARD_ROUTES),
+    // PrimeNg
     SidebarModule,
     ButtonModule,
     ToolbarModule,
     CardModule,
-    ToastModule
+    ToastModule,
+    ChartModule,
+    // Shared
+    SharedModule,
   ],
   providers: [MessageService, CookieService]
 })
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, CurrencyPipe } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ToolbarModule } from 'primeng/toolbar';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { DialogService } from 'primeng/dynamicdialog';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+
+
+@NgModule({
+  declarations: [
+    ToolbarComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    //PrimeNg
+    ToolbarModule,
+    CardModule,
+    ButtonModule,
+  ],
+  exports: [ToolbarComponent],
+  providers: [DialogService, CurrencyPipe]
+})
+export class SharedModule { }
