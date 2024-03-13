3a1f38211646c3feaa85b744a5f09bb152af8c96
Letícia Costa <leticia_603295@trt5.jus.br>
2024-03-13 11:54:38 -0300

    implementando funcionalidade de editar produto

--- src/app/modules/products/components/product-form/product-form.component.ts
+++ src/app/modules/products/components/product-form/product-form.component.ts
@@ -1,23 +1,36 @@
+import { ProductsService } from './../../../../services/products/products.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
+import { DynamicDialogConfig } from 'primeng/dynamicdialog';
 import { Subject, takeUntil } from 'rxjs';
+import { ProductEvent } from 'src/app/models/enums/products/ProductEvent';
 import { GetCategoriesResponse } from 'src/app/models/interfaces/categories/responses/GetGategoriesResponse';
+import { EventAction } from 'src/app/models/interfaces/event/EventAction';
 import { CreateProducRequest } from 'src/app/models/interfaces/products/request/CreateProductRequest';
+import { EditProductRequest } from 'src/app/models/interfaces/products/request/EditProductRequest';
+import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
 import { CategoriesService } from 'src/app/services/categories/categories.service';
-import { ProductsService } from 'src/app/services/products/products.service';
+import { ProductsDataTransferService } from 'src/app/shared/services/products/products-data-transfer.service';
 
 @Component({
   selector: 'app-product-form',
   templateUrl: './product-form.component.html',
   styleUrls: []
 })
-export class ProductFormComponent implements OnInit,OnDestroy {
+export class ProductFormComponent implements OnInit, OnDestroy {
   private readonly destroy$: Subject<void> = new Subject<void>();
 
   public categories: Array<GetCategoriesResponse> = []
   public selectedCategory: Array<{ name: string, code: string}> = []
+  public productAction!: {
+    event: EventAction,
+    productData: Array<GetAllProductsResponse>
+  }
+  public productSelectedData!: GetAllProductsResponse
+  public productsData: Array<GetAllProductsResponse> = []
+
   public addProductForm = this.formBuilder.group({
     name: ['', Validators.required],
     price: ['', Validators.required],
@@ -26,15 +39,44 @@ export class ProductFormComponent implements OnInit,OnDestroy {
     amount: [0, Validators.required],
   })
 
+  public editProductForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    price: ['', Validators.required],
+    description: ['', Validators.required],
+    amount: [0, Validators.required],
+  })
+
+  public addProductAction = ProductEvent.ADD_PRODUCT_EVENT
+  public editProductAction = ProductEvent.EDIT_PRODUCT_EVENT
+  public saleProductAction = ProductEvent.SALE_PRODUCT_EVENT
+
   constructor(
     private categoriesService: CategoriesService,
-    private productService: ProductsService,
+    private productsService: ProductsService,
+    private productsDtService:ProductsDataTransferService,
     private formBuilder: FormBuilder,
     private messageService: MessageService,
     private router: Router,
+    public ref: DynamicDialogConfig,
+
   ) { }
 
   ngOnInit(): void {
+    this.productAction = this.ref.data // inicializa o objeto productAction com os dados passados pelo componente pai - que é o Products-home.component.ts
+
+
+    if (
+      this.productAction?.event?.action === this.editProductAction &&
+      this.productAction?.productData
+    ) {
+      this.getProductSelectedDatas(this.productAction?.event?.id as string);
+    }
+
+    this.productAction?.event?.action === this.saleProductAction &&
+      this.getProductData();
+
+
+
     this.getAllCategories()
   }
 
@@ -59,7 +101,7 @@ export class ProductFormComponent implements OnInit,OnDestroy {
         amount: Number(this.addProductForm.value.amount)
       }
 
-      this.productService.createProduct(requestCreateProduct)
+      this.productsService.createProduct(requestCreateProduct)
       .pipe(takeUntil(this.destroy$))
       .subscribe({
         next: response => {
@@ -84,6 +126,80 @@ export class ProductFormComponent implements OnInit,OnDestroy {
       })
     }
     this.addProductForm.reset()
+  }
+
+  handleSubmitEditProduct(): void {
+    if (
+      this.editProductForm.value &&
+      this.editProductForm.valid &&
+      this.productAction.event.id
+    ) {
+      const requestEditProduct: EditProductRequest = {
+        name: this.editProductForm.value.name as string,
+        price: this.editProductForm.value.price as string,
+        description: this.editProductForm.value.description as string,
+        product_id: this.productAction?.event?.id,
+        amount: this.editProductForm.value.amount as number,
+      };
+
+      this.productsService
+        .editProduct(requestEditProduct)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe({
+          next: () => {
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Sucesso',
+              detail: 'Produto editado com sucesso!',
+              life: 2500,
+            });
+            this.editProductForm.reset();
+          },
+          error: (err) => {
+            console.log(err);
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Erro',
+              detail: 'Erro ao editar produto!',
+              life: 2500,
+            });
+            this.editProductForm.reset();
+          },
+        });
+    }
+  }
+
+  getProductSelectedDatas(productId: string): void {
+    const allProducts = this.productAction?.productData
+
+    if (allProducts.length > 0){
+      const productFiltered = allProducts.filter(
+        product => product?.id === productId)
+
+      if(productFiltered){
+        this.productSelectedData = productFiltered[0]
+
+        this.editProductForm.setValue({
+          name: this.productSelectedData?.name,
+          price: this.productSelectedData?.price,
+          description: this.productSelectedData?.description,
+          amount: this.productSelectedData?.amount
+        })
+      }
+    }
+  }
+
+  getProductData():void{
+    this.productsService.getAllProducts()
+    .pipe(takeUntil(this.destroy$)).
+    subscribe({
+      next: (response) => {
+        if(response.length >0) {
+          this.productsData = response
+          this.productsData && this.productsDtService.setProductsData(this.productsData)
+        }
+      }
+    })
   }
 
   ngOnDestroy(): void {
